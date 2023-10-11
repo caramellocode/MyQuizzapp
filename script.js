9e3f11d909addbc8286e2e0aa8869c4fda301ee9
Marcel Arnold <caramello-code@gmx.ch>
2023-10-11 21:41:45 +0200

    updated functions

--- script.js
+++ script.js
@@ -51,37 +51,29 @@ let questions = [
 
 
 let rightQuestions = 0;
-
-
 let currentQuestion = 0;
 
-
-function init(){
+function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
-
     showQuestion();
 }
 
-
 function showQuestion() {
-    
-
     if (currentQuestion >= questions.length) {
-
-
-        document.getElementById('endScreen').style = '';
-        document.getElementById('questionBody').style = 'display: none';
-
+        document.getElementById('endScreen').style.display = '';
+        document.getElementById('questionBody').style.display = 'none';
         document.getElementById('amount-of-questions').innerHTML = questions.length;
         document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
-        document.getElementById('header-image').src ='img/pokal1.jpg';
-
-
+        document.getElementById('header-image').src = 'img/pokal1.jpg';
+        document.querySelector('.button-row').style.display = 'block';
     } else {
+        document.querySelector('.button-row').style.display = 'none';
+        let percent = currentQuestion === questions.length - 1 ? 100 : Math.round((currentQuestion / questions.length) * 100);
+        document.getElementById('progress-bar').innerHTML = `${percent} %`;
+        document.getElementById('progress-bar').style.width = `${percent}%`;
 
         let question = questions[currentQuestion];
-
-        document.getElementById('question-number').innerHTML = currentQuestion +1;
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
         document.getElementById('questiontext').innerHTML = question['question'];
         document.getElementById('answer_1').innerHTML = question['answer_1'];
         document.getElementById('answer_2').innerHTML = question['answer_2'];
@@ -90,7 +82,7 @@ function showQuestion() {
     }
 }
 
-function answer(selection){
+function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
@@ -98,17 +90,15 @@ function answer(selection){
     if (selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
         rightQuestions++;
-    }   else {
-        console.log('falsche Antwort');
+    } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
-
-
-
     }
+
     document.getElementById('next-button').disabled = false;
 }
-function nextQuestion(){
+
+function nextQuestion() {
     currentQuestion++;
     document.getElementById('next-button').disabled = true;
     resetAnswerButtons();
@@ -116,14 +106,19 @@ function nextQuestion(){
 }
 
 function resetAnswerButtons() {
-    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger', 'bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger', 'bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger', 'bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger', 'bg-success');
 }
 
+function restartGame() {
+    document.getElementById('header-image').src = 'img/quozfoto.jpg';
+    document.getElementById('questionBody').style = '';
+    document.getElementById('endScreen').style = 'display: none';
+    document.getElementById('next-button').disabled = true;
 
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init();
+}
